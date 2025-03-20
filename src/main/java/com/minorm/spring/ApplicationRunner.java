89cba48ffde267082ccdfa27a1d4e590670dd6d6
Minorm-dev <maxim.san231@gmail.com>
2025-03-20 23:18:59 +0300

    lesson-2

--- src/main/java/com/minorm/spring/ApplicationRunner.java
+++ src/main/java/com/minorm/spring/ApplicationRunner.java
@@ -5,23 +5,13 @@ import com.minorm.spring.database.repository.CompanyRepository;
 import com.minorm.spring.database.repository.UserRepository;
 import com.minorm.spring.ioc.Container;
 import com.minorm.spring.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        var container = new Container();
-
-//        var connectionPool = new ConnectionPool();
-//        var userRepository = new UserRepository(connectionPool);
-//        var companyRepository = new CompanyRepository(connectionPool);
-//        var userService = new UserService(userRepository, companyRepository);
-
-        // В данном случае мы не управляем созданием наших объектов
-//        var connectionPool = container.get(ConnectionPool.class);
-//        var userRepository = container.get(UserRepository.class);
-//        var companyRepository = container.get(CompanyRepository.class);
-
-        var userService = container.get(UserService.class);
-        // TODO: 20.03.2025 userService
+        var context = new ClassPathXmlApplicationContext("application.xml");
+        var connectionPool = context.getBean("p1", ConnectionPool.class);
+        System.out.println(connectionPool);
     }
 }
