8622dd140b563b409f67dbfaabb06328f0678a6c
Minorm-dev <maxim.san231@gmail.com>
2025-03-23 23:46:32 +0300

    lesson-9 BeanFactoryPostProcessor

--- src/main/java/com/minorm/spring/ApplicationRunner.java
+++ src/main/java/com/minorm/spring/ApplicationRunner.java
@@ -5,18 +5,25 @@ import com.minorm.spring.database.repository.CompanyRepository;
 import com.minorm.spring.database.repository.UserRepository;
 import com.minorm.spring.ioc.Container;
 import com.minorm.spring.service.UserService;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.Serializable;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        CompanyRepository companyRepository;
+        String value = "hello";
+        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
+        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
+        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
+
         try (var context = new ClassPathXmlApplicationContext("application.xml")) {
             var connectionPool = context.getBean("p1", ConnectionPool.class);
             System.out.println(connectionPool);
 
 
-            companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+            var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
             System.out.println(companyRepository);
         }
     }
