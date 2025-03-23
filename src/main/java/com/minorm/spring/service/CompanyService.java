cc46ab20f52dd3685d0c5353f2cd429e4dbf4488
Minorm-dev <maxim.san231@gmail.com>
2025-03-23 22:50:11 +0300

    lesson-5 property injection

---
+++ src/main/java/com/minorm/spring/service/CompanyService.java
@@ -0,0 +1,10 @@
+package com.minorm.spring.service;
+
+public class CompanyService {
+
+    private UserService userService;
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+}
