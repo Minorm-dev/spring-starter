cf8f5e835e7269554ab265e237bc796c6ecfd9aa
Minorm-dev <maxim.san231@gmail.com>
2025-03-25 00:45:54 +0300

    lesson-20 Import and ImportResource

--- src/main/java/com/minorm/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/minorm/spring/config/ApplicationConfiguration.java
@@ -1,14 +1,14 @@
 package com.minorm.spring.config;
 
 import com.minorm.spring.database.repository.CrudRepository;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.PropertySource;
+import com.minorm.web.config.WebConfiguration;
+import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Component;
 
 import static org.springframework.context.annotation.ComponentScan.*;
 
+//@ImportResource("classpath:application.xml")
+@Import(WebConfiguration.class)
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.minorm.spring",
