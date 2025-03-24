b189fa7d696fb8063142743ab80dcad5784dd8bf
Minorm-dev <maxim.san231@gmail.com>
2025-03-24 22:49:23 +0300

    lesson-12 Custom BeanPostProcessor BeforeInit

--- src/main/java/com/minorm/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/minorm/spring/database/repository/CompanyRepository.java
@@ -1,16 +1,11 @@
 package com.minorm.spring.database.repository;
 
+import com.minorm.spring.bpp.InjectBean;
 import com.minorm.spring.database.pool.ConnectionPool;
 
 public class CompanyRepository {
 
-    private final ConnectionPool connectionPool;
+    @InjectBean
+    private ConnectionPool connectionPool;
 
-    public CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
-
-    public static CompanyRepository of(ConnectionPool connectionPool) {
-        return new CompanyRepository(connectionPool);
-    }
 }
