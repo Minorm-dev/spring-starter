bb343f9e653783d782b6a51ce5775c441243e3a7
Minorm-dev <maxim.san231@gmail.com>
2025-03-24 23:34:39 +0300

    lesson-14 Autowired and Value

--- src/main/java/com/minorm/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/minorm/spring/database/repository/CompanyRepository.java
@@ -5,16 +5,26 @@ import com.minorm.spring.bpp.InjectBean;
 import com.minorm.spring.bpp.Transaction;
 import com.minorm.spring.database.entity.Company;
 import com.minorm.spring.database.pool.ConnectionPool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.util.List;
 import java.util.Optional;
 
 @Transaction
 @Auditing
 public class CompanyRepository implements CrudRepository<Integer, Company> {
 
-    @InjectBean
-    private ConnectionPool connectionPool;
+//    @Resource(name = "pool1")
+//    @Qualifier("pool1")
+    private ConnectionPool pool1;
+    @Autowired
+    private List<ConnectionPool> pools;
+    @Value("${db.pool.size}")
+    private Integer poolSize;
 
     @PostConstruct
     private void init() {
@@ -32,4 +42,8 @@ public class CompanyRepository implements CrudRepository<Integer, Company> {
         System.out.println("delete method....");
     }
 
+    @Autowired
+    public void setPool1(ConnectionPool pool1) {
+        this.pool1 = pool1;
+    }
 }
