f9f30fd20aca3b16a933fecc9e5c75ee291b03ae
Minorm-dev <maxim.san231@gmail.com>
2025-03-28 01:13:15 +0300

    lesson-40 Hibernate Entities

--- src/main/java/com/minorm/spring/ApplicationRunner.java
+++ src/main/java/com/minorm/spring/ApplicationRunner.java
@@ -4,7 +4,6 @@ import com.minorm.spring.config.DatabaseProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
---
+++ src/main/java/com/minorm/spring/database/entity/Role.java
@@ -0,0 +1,5 @@
+package com.minorm.spring.database.entity;
+
+public enum Role {
+    USER, ADMIN
+}
