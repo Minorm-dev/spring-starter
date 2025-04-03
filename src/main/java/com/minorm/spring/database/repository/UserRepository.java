4c4758dd2955a2e9f92128a8fab635721c0e78c2
Minorm-dev <maxim.san231@gmail.com>
2025-04-03 17:26:19 +0300

    lesson-50 @Modifying

--- src/main/java/com/minorm/spring/database/repository/UserRepository.java
+++ src/main/java/com/minorm/spring/database/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.minorm.spring.database.repository;
 
+import com.minorm.spring.database.entity.Role;
 import com.minorm.spring.database.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -17,4 +19,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT u.* FROM users u WHEN u.username = :username",
             nativeQuery = true)
     List<User> findAllByUsername(String username);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update User u " +
+           "set u.role = :role " +
+           "where u.id in (:ids)")
+    int updateRole(Role role, Long... ids);
 }
--- src/test/java/com/minorm/spring/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/com/minorm/spring/integration/database/repository/UserRepositoryTest.java
@@ -1,21 +1,38 @@
 package com.minorm.spring.integration.database.repository;
 
+import com.minorm.spring.database.entity.Role;
 import com.minorm.spring.database.entity.User;
 import com.minorm.spring.database.repository.UserRepository;
 import com.minorm.spring.integration.annotation.IT;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 @IT
 @RequiredArgsConstructor
 class UserRepositoryTest {
 
     private final UserRepository userRepository;
+
+    @Test
+    void checkUpdate() {
+        var ivan = userRepository.getById(1L);
+        assertSame(Role.ADMIN, ivan.getRole());
+        ivan.setBirthDate(LocalDate.now());
+
+        var resultCount = userRepository.updateRole(Role.USER, 1L, 5L);
+        assertEquals(2, resultCount);
+
+//        ivan.getCompany().getName(); // test will be failed because Lazy init.
+
+        var theSameIvan = userRepository.getById(1L);
+        assertSame(Role.USER, theSameIvan.getRole());
+    }
 
     @Test
     void checkQueries(){
