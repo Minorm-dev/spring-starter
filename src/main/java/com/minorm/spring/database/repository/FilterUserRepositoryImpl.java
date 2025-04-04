5a24d2aecec942b7bdec3f09a040754065365e48
Minorm-dev <maxim.san231@gmail.com>
2025-04-05 01:23:12 +0300

    lesson-61 Batch size & Fetch size

--- src/main/java/com/minorm/spring/database/repository/FilterUserRepositoryImpl.java
+++ src/main/java/com/minorm/spring/database/repository/FilterUserRepositoryImpl.java
@@ -8,14 +8,18 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.criteria.Predicate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 @RequiredArgsConstructor
 public class FilterUserRepositoryImpl implements FilterUserRepository {
 
-    private static final String FIND_BY_COMPANY_AND_ROLE= """
+    private static final String FIND_BY_COMPANY_AND_ROLE = """
             SELECT 
                 firstname,
                 lastname,
@@ -25,8 +29,25 @@ public class FilterUserRepositoryImpl implements FilterUserRepository {
             AND role = ?
             """;
 
+    //language=PostgreSQL
+    private static final String UPDATE_COMPANY_AND_ROLE = """
+            UPDATE users
+            SET company_id = ?,
+                role = ?
+            WHERE id = ?
+            """;
+
+    //language=PostgreSQL
+    private static final String UPDATE_COMPANY_AND_ROLE_NAMED = """
+            UPDATE users
+            SET company_id = :company_id,
+                role = :role
+            WHERE id = :id
+            """;
+
     private final EntityManager entityManager;
     private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedJdbcTemplate;
 
     @Override
     public List<User> findAllByFilter(UserFilter filter) {
@@ -42,9 +63,11 @@ public class FilterUserRepositoryImpl implements FilterUserRepository {
         List<Predicate> predicates = new ArrayList<>();
         if (filter.firstname() != null) {
             predicates.add(cb.like(user.get("firstname"), filter.firstname()));
-        }if (filter.lastname() != null) {
+        }
+        if (filter.lastname() != null) {
             predicates.add(cb.like(user.get("lastname"), filter.lastname()));
-        }if (filter.birthDate() != null) {
+        }
+        if (filter.birthDate() != null) {
             predicates.add(cb.lessThan(user.get("birthDate"), filter.birthDate()));
         }
 
@@ -57,9 +80,30 @@ public class FilterUserRepositoryImpl implements FilterUserRepository {
     public List<PersonalInfo> findAllByCompanyIdAndRole(Integer companyId, Role role) {
         return jdbcTemplate.query(FIND_BY_COMPANY_AND_ROLE,
                 (rs, rowNum) -> new PersonalInfo(
-                rs.getString("firstname"),
-                rs.getString("lastname"),
-                rs.getDate("birth_date").toLocalDate()
-        ), companyId, role.name());
+                        rs.getString("firstname"),
+                        rs.getString("lastname"),
+                        rs.getDate("birth_date").toLocalDate()
+                ), companyId, role.name());
+    }
+
+    @Override
+    public void updateCompanyAndRole(List<User> users) {
+        var args = users.stream()
+                .map(user -> new Object[]{user.getCompany().getId(), user.getRole().name(), user.getId()})
+                .toList();
+        jdbcTemplate.batchUpdate(UPDATE_COMPANY_AND_ROLE, args);
+    }
+
+    @Override
+    public void updateCompanyAndRoleNamed(List<User> users) {
+        var args = users.stream()
+                .map(user -> Map.of(
+                        "companyId", user.getCompany().getId(),
+                        "role", user.getRole().name(),
+                        "id", user.getId()
+                ))
+                .map(MapSqlParameterSource::new)
+                .toArray(MapSqlParameterSource[]::new);
+        namedJdbcTemplate.batchUpdate(UPDATE_COMPANY_AND_ROLE_NAMED, args);
     }
 }
