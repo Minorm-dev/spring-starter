3f4cf8163bf7b99dcbd8c8701d245dfcc741ffa4
Minorm-dev <maxim.san231@gmail.com>
2025-04-05 00:45:17 +0300

    lesson-59 Querydsl

---
+++ src/main/java/com/minorm/spring/database/querydsl/QPredicates.java
@@ -0,0 +1,29 @@
+package com.minorm.spring.database.querydsl;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class QPredicates {
+
+//    private final List<Predicate> predicates = new ArrayList<>();
+//
+//    public static QPredicates builder() {
+//        return new QPredicates();
+//    }
+//
+//    public <T> QPredicates add(T object, Function<T, Predicate> function) {
+//        if (object != null) {
+//            predicates.add(function.apply(object));
+//        }
+//        return this;
+//    }
+//
+//    public Predicate build() {
+//        return ExpressionUtils.allOf(predicates);
+//    }
+//
+//    public Predicate buildOr() {
+//        return ExpressionUtils.anyOf(predicates);
+//    }
+}
--- src/main/java/com/minorm/spring/database/repository/FilterUserRepositoryImpl.java
+++ src/main/java/com/minorm/spring/database/repository/FilterUserRepositoryImpl.java
@@ -3,10 +3,7 @@ package com.minorm.spring.database.repository;
 import com.minorm.spring.database.entity.User;
 import com.minorm.spring.dto.UserFilter;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 
 import java.util.ArrayList;
@@ -19,6 +16,19 @@ public class FilterUserRepositoryImpl implements FilterUserRepository {
 
     @Override
     public List<User> findAllByFilter(UserFilter filter) {
+//        var predicate = QPredicates.builder()
+//                .add(filter.firstname(), user.firstname::containsIgnoreCase)
+//                .add(filter.lastname(), user.lastname::containsIgnoreCase)
+//                .add(filter.birthDate(), user.birthDate::before)
+//                .build();
+//        return new JPAQuery<User>(entityManager)
+//                .select(user)
+//                .from(user)
+//                .where(predicate)
+//                .fetch();
+//    }
+
+
         var cb = entityManager.getCriteriaBuilder();
         var criteria = cb.createQuery(User.class);
 
