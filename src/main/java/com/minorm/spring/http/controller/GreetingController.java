04130ef076fb4b824a47fd23ed96a8b873f71d25
Minorm-dev <maxim.san231@gmail.com>
2025-04-05 21:39:45 +0300

    lesson-67 Parameters, Headers, Cookies

--- src/main/java/com/minorm/spring/http/controller/GreetingController.java
+++ src/main/java/com/minorm/spring/http/controller/GreetingController.java
@@ -1,26 +1,30 @@
 package com.minorm.spring.http.controller;
 
-import com.minorm.spring.database.repository.CompanyRepository;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/api/v1")
 public class GreetingController {
 
-//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    @GetMapping("/hello")
-    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request, CompanyRepository companyRepository) {
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request,
+                              @RequestParam Integer age,
+                              @RequestHeader String accept,
+                              @CookieValue("JSESSIONID") String jsessionId,
+                              @PathVariable Integer id) {
+        String ageParamValue = request.getParameter("age");
+        String acceptHeader = request.getHeader("accept");
+        Cookie[] cookies = request.getCookies();
+
         modelAndView.setViewName("greeting/hello");
 
         return modelAndView;
     }
 
-//    @RequestMapping(value = "/bye", method = RequestMethod.GET)
     @GetMapping("/bye")
     public ModelAndView bye() {
         ModelAndView modelAndView = new ModelAndView();
