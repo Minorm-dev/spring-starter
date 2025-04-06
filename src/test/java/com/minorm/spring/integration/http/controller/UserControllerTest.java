d2e42b018e1cd4810c6d2e1554fe1b22f66b93d0
Minorm-dev <maxim.san231@gmail.com>
2025-04-06 17:52:22 +0300

    lesson-73 Spring MVC Testing

---
+++ src/test/java/com/minorm/spring/integration/http/controller/UserControllerTest.java
@@ -0,0 +1,49 @@
+package com.minorm.spring.integration.http.controller;
+
+import com.minorm.spring.dto.UserCreateEditDto;
+import com.minorm.spring.integration.IntegrationTestBase;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.shaded.org.hamcrest.collection.IsCollectionWithSize;
+
+import static com.minorm.spring.dto.UserCreateEditDto.Fields.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.testcontainers.shaded.org.hamcrest.collection.IsCollectionWithSize.*;
+
+@AutoConfigureMockMvc // имитация http запросов в приложении
+@RequiredArgsConstructor
+class UserControllerTest extends IntegrationTestBase {
+
+    private final MockMvc mockMvc;
+
+    @Test
+    void findAll() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("user/users"))
+                .andExpect(model().attributeExists("users"));
+//                .andExpect(model().attribute("users", hasSize(5)));
+    }
+
+    @Test
+    void create() throws Exception {
+        mockMvc.perform(post("/users")
+                .param(username, "test@gmail.com")
+                .param(firstname, "test")
+                .param(lastname, "test")
+                .param(role, "ADMIN")
+                .param(companyId, "1")
+//                        .param(birthDate, "2000-01-01")
+        )
+                .andExpectAll(
+                        status().is3xxRedirection(),
+                        redirectedUrlPattern("/users/{\\d+}")
+                );
+    }
+}
