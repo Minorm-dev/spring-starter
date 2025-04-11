1f82f4b9eb97205c17cca9bac2306c107ea9addf
Minorm-dev <maxim.san231@gmail.com>
2025-04-11 18:24:29 +0300

    lesson-88 Form Login

--- src/main/java/com/minorm/spring/http/controller/LoginController.java
+++ src/main/java/com/minorm/spring/http/controller/LoginController.java
@@ -15,9 +15,5 @@ public class LoginController {
         return "user/login";
     }
 
-    @PostMapping("/login")
-    public String login(Model model, @ModelAttribute("login") LoginDto loginDto) {
-//        return "redirect:https://google.com";
-        return "redirect:/login";
-    }
+
 }
